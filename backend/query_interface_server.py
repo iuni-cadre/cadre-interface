3a1939766b92468441d4fe348def5f6b9fb0dbf5
Ben Serrette <bserrett@iu.edu>
2019-02-11 12:45:30 -0500

    Cleans up some comments.

--- backend/query_interface_server.py
+++ backend/query_interface_server.py
@@ -1,16 +1,19 @@
+
+# Flask app whose primary purpose is to server the frontend
+
 from flask import Flask, render_template
 app = Flask(__name__, 
     template_folder="../frontend/dist",
     static_folder="../frontend/dist/assets"
     )
 
-# frontend_path = ""
-
 @app.route("/")
 def index():
-    # path = frontend_path + "/index.html"
     return render_template("/index.html")
-    # return "Hello World!"
+
+@app.route("/<path:fallback>")
+def fallback(fallback):
+    return render_template("/index.html")
 
 if __name__ == '__main__':
     app.run(host='localhost', port=5000, debug=True)
