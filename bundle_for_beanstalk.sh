d52f79fae0093c9a5efd5e4ff27b7d2f3b2c8a23
Ben Serrette <bserrett@iu.edu>
2019-03-26 14:26:49 -0400

    Changes to beanstalk deploy

--- bundle_for_beanstalk.sh
+++ bundle_for_beanstalk.sh
@@ -25,12 +25,13 @@ pushd bundle
 
 rm -rf ../eb_bundle*.zip
 today=`date '+%Y-%m-%d--%H-%M-%S'`
-zip -r ../eb_bundle-$today.zip -r * .[^.]*
+zip -r ../eb_bundle.zip -r * .[^.]*
+# zip -r ../eb_bundle-$today.zip -r * .[^.]*
 
 #remove the temporary bundle directory
 pushd ..
 rm -rf ./bundle
 
-# exec eb deploy -v &
-# wait
-# exec eb status -v 
+exec eb deploy -v &
+wait
+exec eb status -v 
