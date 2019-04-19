b9f7d1ed7ff360376f7712bfcb869c1d70404eb9
Ben Serrette <bserrett@iu.edu>
2019-04-19 14:41:00 -0400

    Makes QI and Notebook links pop up in new windows.

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -5,9 +5,13 @@
         <hr />
 
         <div>
-            <button @click="goToQueryInterface()" class="btn btn-primary">Query Interface</button>
+            <!-- <button @click="goToQueryInterface()" class="btn btn-primary">Query Interface</button>
             &nbsp;&nbsp;
-            <button @click="goToNotebook()" class="btn btn-primary">Jupyter Notebook</button>
+            <button @click="goToNotebook()" class="btn btn-primary">Jupyter Notebook</button> -->
+            <a target="_blank" :href="`${this.query_interface_url}?username=${this.username}&token=${this.token}`" class="btn btn-primary">Query Interface</a>
+            &nbsp;&nbsp;
+            <!-- <a :href="" class="btn btn-primary">Jupyter Notebook</a> -->
+            <router-link :to="{name: 'jupyter-hub'}" target="_blank" class="btn btn-primary">Jupyter Notebook</router-link>
         </div>
         <!-- <div>
             {{jupyter_full_url}}
