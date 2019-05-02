16b6e703fd615fdae394595006343f20ef44aec9
Ben Serrette <bserrett@iu.edu>
2019-05-02 15:30:58 -0400

    Moves query builder beanstalk deploy scripts

--- deploy_to_beanstalk.sh
+++ deploy_to_beanstalk.sh
@@ -9,12 +9,17 @@ exec ./package.sh &
 wait
 pushd ..
 
-rm -rf eb_bundle*.zip
+rm -rf ../eb_bundle*.zip
 
 pushd bundle
 today=`date '+%Y-%m-%d--%H-%M-%S'`
-zip -r ../eb_bundle-$today.zip -r * .[^.]*
+zip -r ../eb_bundle.zip -r * .[^.]*
+# zip -r ../eb_bundle-$today.zip -r * .[^.]*
 
 #remove the temporary bundle directory
 pushd ..
 rm -rf ./bundle
+
+exec eb deploy -v &
+wait
+exec eb status -v 
