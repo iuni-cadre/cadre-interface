071b3675977ab5a02e20cce813939fd1b92a54d7
Ben Serrette <bserrett@iu.edu>
2019-10-16 17:54:37 -0400

    Adds filesystem store.

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    verbose: false,
+    verbose: true,
     moduleFileExtensions: [
         "js",
         "json",
@@ -7,7 +7,7 @@ module.exports = {
     ],
     transform: {
         ".*\\.(vue)$": "vue-jest",
-        "^.+\\.js$": "<rootDir>/node_modules/babel-jest"
+        "^.+\\.js$": "babel-jest"
     },
     collectCoverage: false,
     collectCoverageFrom: [
@@ -18,4 +18,25 @@ module.exports = {
         "html",
         "text-summary"
     ],
+    transformIgnorePatterns: ["/node_modules/"]
+    // "transformIgnorePatterns": [
+    //     "node_modules/(?!@ngrx|(?!deck.gl)|ng-dynamic)"
+    //   ]
 }
+
+// module.exports = {
+//     moduleFileExtensions: ["js", "jsx", "json", "vue"],
+//     transform: {
+//         "^.+\\.vue$": "vue-jest",
+//         ".+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$": "jest-transform-stub",
+//         "^.+\\.jsx?$": "babel-jest"
+//     },
+//     transformIgnorePatterns: ["/node_modules/"],
+//     moduleNameMapper: {
+//         "^@/(.*)$": "<rootDir>/src/$1"
+//     },
+//     snapshotSerializers: ["jest-serializer-vue"],
+//     // testMatch: ["**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)"],
+//     testURL: "http://localhost/",
+//     watchPlugins: ["jest-watch-typeahead/filename", "jest-watch-typeahead/testname"]
+// };
--- frontend/src/store/modules/racpackage.js
+++ frontend/src/store/modules/racpackage.js
@@ -41,13 +41,13 @@ export default {
             }
         }
     },
-    //    ###     ######  ######## ####  #######  ##    ##  ######
-    //   ## ##   ##    ##    ##     ##  ##     ## ###   ## ##    ##
-    //  ##   ##  ##          ##     ##  ##     ## ####  ## ##
-    // ##     ## ##          ##     ##  ##     ## ## ## ##  ######
-    // ######### ##          ##     ##  ##     ## ##  ####       ##
-    // ##     ## ##    ##    ##     ##  ##     ## ##   ### ##    ##
-    // ##     ##  ######     ##    ####  #######  ##    ##  ######
+//     //    ###     ######  ######## ####  #######  ##    ##  ######
+//     //   ## ##   ##    ##    ##     ##  ##     ## ###   ## ##    ##
+//     //  ##   ##  ##          ##     ##  ##     ## ####  ## ##
+//     // ##     ## ##          ##     ##  ##     ## ## ## ##  ######
+//     // ######### ##          ##     ##  ##     ## ##  ####       ##
+//     // ##     ## ##    ##    ##     ##  ##     ## ##   ### ##    ##
+//     // ##     ##  ######     ##    ####  #######  ##    ##  ######
     actions: {
         getTools: function({ commit }) {
             return new Promise((resolve, reject) => {
@@ -120,7 +120,7 @@ export default {
                         order: "name",
                         search: ""
                     },
-                    responseType: 'json'
+                    responseType: "json"
                 });
                 axios_prom.then(
                     response => {
@@ -135,7 +135,7 @@ export default {
                 );
             });
         },
-        runPackage: function({}, { package_id, input_files, output_filenames }) {
+        runPackage: function(context, { package_id, input_files, output_filenames }) {
             return new Promise((resolve, reject) => {
                 let axios_prom = Vue.$cadre.axios({
                     url: "/rac-api/packages/run-package",
@@ -166,7 +166,7 @@ export default {
                 );
             });
         },
-        createPackage: function({}, { package_name, tool_id, input, description }) {
+        createPackage: function(context, { package_name, tool_id, input, description }) {
             return new Promise((resolve, reject) => {
                 let axios_prom = Vue.$cadre.axios({
                     url: "/rac-api/packages/new-package",
@@ -198,172 +198,3 @@ export default {
         }
     }
 };
-
-const TEST_RAC_PACKAGES = [
-
-    {
-        package_id: "234221136",
-        name: "ISSI Data Package",
-        author: "CADRE Team",
-        created_date: "2019-08-26 16:32:48",
-        tools: ["11234221128"],
-        input_files: ["data.tar"]
-        // output_files: 2
-    },
-    {
-        package_id: "234221133",
-        name: "Query 2 Xnet Package",
-        author: "CADRE Team",
-        created_date: "2019-08-22 13:37:15",
-        tools: ["11234221125"],
-        input_files: ["/dataset.csv", "/dataset_edges.csv"]
-        // output_files: 1 // extension .xnet
-    },
-    {
-        package_id: "234221135",
-        name: "Xnet Word Cloud",
-        author: "CADRE Team",
-        created_date: "2019-08-22 13:37:15",
-        tools: ["11234221127"],
-        input_files: ["results.xnet"]
-    },
-    {
-        package_id: "234221137",
-        name: "Xnet2 Figure Package ",
-        author: "CADRE Team",
-        created_date: "2019-08-22 13:37:15",
-        tools: ["11234221129"],
-        input_files: ["results.xnet"]
-    },
-    {
-        package_id: "234221134",
-        name: "Xnet2 Communities Wordcloud Package",
-        author: "CADRE Team",
-        created_date: "2019-08-27 14:13:28",
-        tools: ["11234221126"],
-        input_files: ["results.xnet"]
-    },
-    {
-        package_id: "234221132",
-        name: "Line Count Package",
-        author: "CADRE Team",
-        created_date: "2019-07-16 10:51:26",
-        tools: ["11234221124"],
-        input_files: ["/dataset1.txt", "/dataset2.txt"]
-        // output_files: 2
-    },
-
-
-
-
-
-
-    // {
-    //     package_id: "234221134",
-    //     name: "Demo Download Package",
-    //     author: "CADRE Team",
-    //     created_date: "2019-08-22 15:04:43",
-    //     tools: ["11234221126"],
-    //     input_files: []
-    //     // output_files: 1 // extension .xnet
-    // }
-    // {
-    //     package_id: "package_2",
-    //     name: "package 2",
-    //     author: "author 2",
-    //     created_date: "2019-07-16 10:51:26",
-    //     tools: ["tool_2"],
-    //     input_files: ["/dataset.csv"],
-    //     output_files: ["/new_dataset.csv"]
-    // },
-    // {
-    //     package_id: "package_3",
-    //     name: "package 3",
-    //     author: "author 3",
-    //     created_date: "2019-07-16 10:51:26",
-    //     tools: ["tool_3"],
-    //     input_files: ["/dataset.csv"],
-    //     output_files: ["/new_dataset.csv"]
-    // }
-];
-
-const TEST_RAC_TOOLS = [
-    {
-        tool_id: "11234221124",
-        name: "Line Count",
-        author: "CADRE Team",
-        description: "Counts lines in a file",
-        output_files: ["file_1.csv", "file_2.csv"]
-    },
-    {
-        tool_id: "11234221125",
-        name: "query2_xnet",
-        author: "CADRE Team",
-        description: "Sends query to xnet package.",
-        output_files: ["result.xnet"]
-    },
-    {
-        tool_id: "11234221126",
-        name: "xnet2_communities",
-        author: "CADRE Team",
-        description: "Generate word cloud for all the different xnet communites.",
-        output_files: ["word_cloud.pdf"]
-    },
-    {
-        tool_id: "11234221127",
-        name: "xnet2_wordcloud",
-        author: "CADRE Team",
-        description: "Sends query to xnet package.",
-        output_files: ["word_cloud.pdf"]
-    },
-    {
-        tool_id: "11234221128",
-        name: "issi_data_package",
-        author: "CADRE Team",
-        description: "Get my data ready for the demo",
-        output_files: ["data"]
-    },
-    {
-        tool_id: "11234221129",
-        name: "xnet2_figure_package ",
-        author: "CADRE Team",
-        description: "convert my xnet file to a figure",
-        output_files: ["figure.pdf"]
-    },
-    // {
-    //     tool_id: "11234221126",
-    //     name: "Xiaoran's Demo",
-    //     author: "CADRE Team",
-    //     description: "Downloads demo files to user workspace.",
-    //     output_files: ["demo_folder"]
-    // },
-
-
-    // {
-    //     tool_id: "tool_2",
-    //     name: "Word Count",
-    //     author: "CADRE Team",
-    //     description: "Counts words in a file"
-    // },
-    // {
-    //     tool_id: "tool_3",
-    //     name: "Graph",
-    //     author: "CADRE Team",
-    //     description: "Generates a graph"
-    // }
-];
-
-const TEST_RAC_JOBS = [
-    {
-        job_id: "job1"
-    },
-    {
-        job_id: "job2"
-    },
-    {
-        job_id: "job3"
-    },
-    {
-        job_id: "job4"
-    }
-];
