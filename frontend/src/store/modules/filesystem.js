91752b3f1efa711da73efb9c64e7b81d0fe15b6e
Ben Serrette <bserrett@iu.edu>
2019-10-17 12:53:50 -0400

    Gets JEST to work with Async.  Adds tests for getfiles action.

--- frontend/src/store/modules/filesystem.js
+++ frontend/src/store/modules/filesystem.js
@@ -2,24 +2,27 @@ import Globals from "../../CadreGlobalsPlugin";
 import axios from "axios";
 import Vue from "vue";
 
-
 export default {
     namespaced: true,
     state: {
         root: ""
     },
     getters: {
-        "getRoot": function(state) {
+        getRoot: function(state) {
             return state.root;
         }
     },
     mutations: {
-        "setRoot": function(state, root)
-        {
+        setRoot: function(state, root) {
             state.root = root;
         }
     },
     actions: {
-
+        getFiles: function() {
+            // return 'test';
+            return new Promise((resolve, reject) => {
+                reject({error: "balls"});
+            });
+        }
     }
 };
