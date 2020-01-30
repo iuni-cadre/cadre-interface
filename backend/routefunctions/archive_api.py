22006c85aec64da9197dc708a1249a40a63857e4
cpelikan <cpelikan@iu.edu>
2020-01-30 14:35:11 -0500

    get_archive endpoint

--- backend/routefunctions/archive_api.py
+++ backend/routefunctions/archive_api.py
@@ -421,3 +421,114 @@ def delete_archive():
         cur.close()
         conn.close()
         print("The database connection has been closed successfully.")
+
+
+@blueprint.route('/rac-api/get-archives/user', methods=['GET'])
+def get_archives():
+    """
+    This is a method which returns the details of all the archives for the user.
+
+    Args:
+
+    Returns:
+        This method returns a json object containing the details of all the archives.
+    """
+    auth_token = request.headers.get('auth-token')
+    username = request.headers.get('auth-username')
+    limit = int(request.args.get('limit', 25))
+    page = int(request.args.get('page', 0))
+    order = request.args.get('order', 'name')
+    search = request.args.get('search', '')
+
+    headers = {
+        'auth-token': auth_token,
+        'auth-username': username
+    }
+    is_valid, validate_token_response = utilities.validate_user(headers=headers)
+    if not is_valid:
+        return validate_token_response
+
+    validate_response_json = None
+    try:
+        validate_response_json = validate_token_response.get_json()
+    except:
+        validate_response_json = validate_token_response.json()
+    user_id = validate_response_json.get("user_id", None)
+
+    if not user_id:
+        return jsonify({"error": "Invalid user"}), 401
+
+    # Validating the Request here
+    try:
+        limit_value = int(limit)
+        if limit_value > 0:
+            print("Yes limit is a positive integer.")
+            print("The value of limit is: ", limit_value)
+    except ValueError:
+        print("No Limit is not an Integer. It's a string")
+        return jsonify({"error": "Invalid Request: Limit should be a positive integer."}), 400
+
+    try:
+        page_value = int(page)
+        if page_value >= 0:
+            print("Yes page is an Integer.")
+            print("The value of page is: ", page_value)
+    except ValueError:
+        print("No Page is not an Integer. It's a string")
+        return jsonify({"error": "Invalid Request: Page should be a integer."}), 400
+
+    # This prevents sql injection for the order by clause. Never use data sent by the user directly in a query
+    actual_order_by = 'name'
+    if order == 'name':
+        actual_order_by = 'name'
+    if order == 'description':
+        actual_order_by = 'description'
+    if order == 'created_on':
+        actual_order_by = 'created_on'
+
+    offset = page * limit
+
+    # This is where we are actually connecting to the database and getting the details of the archives
+    conn = psycopg2.connect(dbname=meta_db_config["database-name"], user=meta_db_config["database-username"],
+                            password=meta_db_config["database-password"], host=meta_db_config["database-host"],
+                            port=meta_db_config["database-port"])
+    cur = conn.cursor()
+
+    # Here we are getting all the details of the all the different archives from the database
+    try:
+        query = """SELECT
+                archive_id as archive_id,
+                s3_location as s3_location,
+                description as archive_description,
+                name as archive_name,
+                created_on as archive_created_on,
+                created_by as created_by
+                FROM archive
+                WHERE to_be_deleted IS NOT TRUE 
+                AND (published IS TRUE OR created_by = %s)
+                ORDER BY {}
+                LIMIT %s
+                OFFSET %s; """.format(actual_order_by)
+
+        cur.execute(query, (user_id, limit, offset))
+
+        archive_info = cur.fetchall()
+        archive_list = []
+        for archives in archive_info:
+            archive_json = {
+                'archive_id': archives[0],
+                'archive_s3_location': archives[1],
+                'archive_description': archives[2],
+                'archive_name': archives[3],
+                'created_on': archives[4].isoformat(),
+                'created_by': archives[5]
+            }
+            archive_list.append(archive_json)
+        return jsonify(archive_list), 200
+    except Exception:
+        return jsonify({"error:", "Problem querying the archives table in the meta database."}), 500
+    finally:
+        # Closing the database connection.
+        cur.close()
+        conn.close()
+        print("The database connection has been closed successfully.")
