f3ea0949baa30c87293e006ef7fbf8f200185f1b
Ben Serrette <bserrett@iu.edu>
2020-02-06 17:29:42 -0500

    Handles permission denied errors for user files.

--- backend/routefunctions/rac_api.py
+++ backend/routefunctions/rac_api.py
@@ -795,22 +795,24 @@ def get_user_files():
 
         if path is not "/":
             path = path + "/"
-            
-        print(efs_path)
-        print(username)
-        print(path)
-        print(directory_path)
 
-        for item in os.listdir(directory_path):
-            # print(item)
-            if os.path.isfile(os.path.join(directory_path, item)):
-                path_from_home = path +  item #os.path.join(root, item).replace(directory_path, '', 1)
-                file_info.append({'path': '{}'.format(path_from_home), 'type': 'file'})
-            else:
-                
-                path_from_home = path +  item #os.path.join(root, item).replace(directory_path, '', 1)
-                file_info.append({'path': '{}'.format(path_from_home), 'type': 'folder'})
+        # print(efs_path)
+        # print(username)
+        # print(path)
+        # print(directory_path)
 
+        try:
+            for item in os.listdir(directory_path):
+                # print(item)
+                if os.path.isfile(os.path.join(directory_path, item)):
+                    path_from_home = path +  item #os.path.join(root, item).replace(directory_path, '', 1)
+                    file_info.append({'path': '{}'.format(path_from_home), 'type': 'file'})
+                else:
+                    
+                    path_from_home = path +  item #os.path.join(root, item).replace(directory_path, '', 1)
+                    file_info.append({'path': '{}'.format(path_from_home), 'type': 'folder'})
+        except PermissionError as err:
+            return jsonify({"error": "Permission Denied.", "message": str(err)}), 403
 
 
         # for root, dirs, files in os.walk(directory_path):
